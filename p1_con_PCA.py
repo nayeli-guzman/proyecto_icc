92b306dc74c04611b2ea5422911827df2f579f58
LuisM <luis.maquera@utec.edu.pe>
2024-10-24 00:10:46 -0500

    PCA2

--- p1_con_PCA.py
+++ p1_con_PCA.py
@@ -2,26 +2,44 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
-misDatos = pd.read_csv('pokemons agrupados.csv')
-# print(misDatos.columns[0])
+# Cargamos en un DataFrame el archivo CSV generado en p1
+misDatos = pd.read_csv('pokemonesAgrupados1.csv')
+data = pd.read_csv("smogon.csv")
+pokemones_clusters_sinPCA = pd.read_csv("pokemonesClusters1.csv")
+
 # Eliminamos la columna indice y cluster
-
-misDatos.drop(['Cluster'], axis=1, inplace=True)
 misDatos.drop(misDatos.columns[0], axis=1, inplace=True)
+misDatos.drop(['Cluster'], axis=1, inplace=True)
+print(misDatos)
 
-# Encontramos la matriz de PCA
-pca = PCA(n_components=10)
+# Elaboramos la matriz de PCA
+pca = PCA(n_components=12)
 matriz_pca = pca.fit_transform(misDatos)
+print("Filas y columnas del dataframe original: ",misDatos.shape)
+print("Filas y columnas de la matriz de PCA: ",matriz_pca.shape)
 
-
-# Hacemos Kmedias
-km = KMeans(n_clusters=17)
-lista = km.fit_predict(matriz_pca)
-
+# Generacion del nuevo Dataframe
 cabeceras = ["PCA 1", "PCA 2", "PCA 3", "PCA 4", "PCA 5", "PCA 6", "PCA 7", "PCA 8", "PCA 9", "PCA 10"]
+matrizPCA = pd.DataFrame(matriz_pca, columns=cabeceras)
+
+# Empleamos Kmedias para agrupar las filas
+km = KMeans(n_clusters=18)
+clusters_con_PCA = km.fit_predict(matriz_pca)
+
 
 #Creamos un CSV con los PCA y los clusters
-matrizPCA = pd.DataFrame(matriz_pca, columns=cabeceras)
-matrizPCA['Cluster'] = lista
+matrizPCA['Cluster'] = clusters_con_PCA
 matrizPCA.to_csv('pokemons agrupados con PCA.csv')
+
+table = pd.DataFrame({
+    "Pokemon": data["Pokemon"],
+    "Cluster": clusters_con_PCA
+})
+table.to_csv("pokemonesClusters3.csv")
+
+from sklearn.metrics import adjusted_rand_score
+
+# Comparación usando ARI
+ari_score = adjusted_rand_score(pokemones_clusters_sinPCA["Cluster"], clusters_con_PCA)
+print(f"ARI Score: {ari_score}")
 
